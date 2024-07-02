06fe1a0ef6f7f432bdbcbe1834e34ef9605bff67
habaneraa <Lumberjack1024@yeah.net>
2024-07-02 12:45:52 +0800

    fix url base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/test_graph_site/',
+  base: '/cwe-navigation/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
